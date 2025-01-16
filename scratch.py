6160fa2063ac2c6ce5b0879c2f830ce7025cdb99
iraros <irarosenblum2@gmail.com>
2025-01-17 01:50:35 +0200

    start

---
+++ scratch.py
@@ -0,0 +1,12 @@
+import pickle
+
+from logic import get_word_times
+
+pkl_save_path = r''
+
+with open(pkl_save_path, "rb") as file:
+    result = pickle.load(file)
+
+for segment in result["segments"]:
+    start, end = get_word_times(segment)
+    print(start, segment["text"])
